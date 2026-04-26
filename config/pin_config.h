c9e5a590a6220deadcc64fdee02ca3fb4210c512
Hamid Mustafa <eng.hammedsaed396@gmail.com>
2026-04-26 15:19:25 +0300

    update Config for Ir sensor Array

--- config/pin_config.h
+++ config/pin_config.h
@@ -9,14 +9,24 @@
 // IR Sensors (PORTA)
 #define IR_TRIS         TRISA
 #define IR_PORT         PORTA
-// Note: We use RA0, RA1, RA2, RA3, RA5
+#define IR_SENSOR_1     RA0_bit
+#define IR_SENSOR_2     RA1_bit
+#define IR_SENSOR_3     RA2_bit
+#define IR_SENSOR_4     RA3_bit
+#define IR_SENSOR_5     RA4_bit
+// Note: We use RA0, RA1, RA2, RA3, RA4 for IR sensors, so bits 0-4 of PORTA are used for IR input
 
 // Motors Direction (PORTB)
 #define MOTOR_DIR_TRIS  TRISB
 #define L_IN1           RB0_bit
 #define L_IN2           RB1_bit
+
 #define R_IN3           RB2_bit
 #define R_IN4           RB3_bit
+//definre L_PWM ,define R_PWM          RC2_bit, RC1_bit
+#define L_PWM           RC2_bit
+#define R_PWM           RC1_bit
+
 
 // Motors PWM (PORTC) - Handled by MikroC PWM Library
 // Left = RC2 (CCP1), Right = RC1 (CCP2)
@@ -24,7 +34,8 @@
 // Ultrasonic (PORTB)
 #define TRIG_PIN        RB4_bit
 #define ECHO_PIN        RB5_bit
-#define TRIG_TRIS       TRISB4_bit
+// TRIS for Ultrasonic
+#define TRIG_TRIS       TRISB4_bit 
 #define ECHO_TRIS       TRISB5_bit
 
 // Buzzer (PORTC)
@@ -38,7 +49,7 @@ sbit LCD_D4 at RD4_bit;
 sbit LCD_D5 at RD5_bit;
 sbit LCD_D6 at RD6_bit;
 sbit LCD_D7 at RD7_bit;
-
+// LCD TRIS
 sbit LCD_RS_Direction at TRISD0_bit;
 sbit LCD_EN_Direction at TRISD1_bit;
 sbit LCD_D4_Direction at TRISD4_bit;
