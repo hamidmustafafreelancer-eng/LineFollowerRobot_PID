e1bd0414e7cf1bc9d3f1f67ddb6e083e003cbf66
Hamid Mustafa <eng.hammedsaed396@gmail.com>
2026-05-01 16:24:28 +0300

    Move IR sensors to PORTB (RB0 → RB4),Motors + Ultrasonic
    So we rearrange smartly.

--- config/pin_config.h
+++ config/pin_config.h
@@ -6,42 +6,44 @@
 #ifndef PIN_CONFIG_H
 #define PIN_CONFIG_H
 
-// IR Sensors (PORTA)
-#define IR_TRIS         TRISA
-#define IR_PORT         PORTA
-#define IR_SENSOR_1     RA0_bit
-#define IR_SENSOR_2     RA1_bit
-#define IR_SENSOR_3     RA2_bit
-#define IR_SENSOR_4     RA3_bit
-#define IR_SENSOR_5     RA5_bit  // Use RA5, avoid RA4 which is often used for other purposes (like ADC or I/O)
-// Note: We use RA0, RA1, RA2, RA3, RA4 for IR sensors, so bits 0-4 of PORTA are used for IR input
+// IR Sensors (PORTB - Consecutive) 
+#define IR_TRIS         TRISB
+#define IR_PORT         PORTB
 
-// Motors Direction (PORTB)
-#define MOTOR_DIR_TRIS  TRISB
-#define L_IN1           RB0_bit
-#define L_IN2           RB1_bit
+#define IR_SENSOR_1     RB0_bit
+#define IR_SENSOR_2     RB1_bit
+#define IR_SENSOR_3     RB2_bit
+#define IR_SENSOR_4     RB3_bit
+#define IR_SENSOR_5     RB4_bit
 
-#define R_IN3           RB2_bit
-#define R_IN4           RB3_bit
+#define IR_MASK         0x1F   // 00011111
+
+// Motors Direction (PORTC)
+#define MOTOR_DIR_TRIS  TRISC
+
+#define L_IN1           RC3_bit
+#define L_IN2           RC4_bit
+
+#define R_IN3           RC5_bit
+#define R_IN4           RC6_bit
+
 //definre L_PWM ,define R_PWM          RC2_bit, RC1_bit
 #define L_PWM           RC2_bit
 #define R_PWM           RC1_bit
-
-
 // Motors PWM (PORTC) - Handled by MikroC PWM Library
 // Left = RC2 (CCP1), Right = RC1 (CCP2)
-
+#define L_PWM           RC2_bit   // CCP1
+#define R_PWM           RC1_bit   // CCP2
 // Ultrasonic (PORTB)
-#define TRIG_PIN        RB4_bit
-#define ECHO_PIN        RB5_bit
+#define TRIG_PIN        RB5_bit
+#define ECHO_PIN        RB6_bit
 // TRIS for Ultrasonic
-#define TRIG_TRIS       TRISB4_bit 
-#define ECHO_TRIS       TRISB5_bit
+#define TRIG_TRIS       TRISB5_bit
+#define ECHO_TRIS       TRISB6_bit
 
 // Buzzer (PORTC)
 #define BUZZER_PIN      RC0_bit
 #define BUZZER_TRIS     TRISC0_bit
-
 // LCD Pinout (Standard MikroC Lcd Connection)
 sbit LCD_RS at RD0_bit;
 sbit LCD_EN at RD1_bit;
